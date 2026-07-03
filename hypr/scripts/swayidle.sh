a2885ab0bd954c85c9a81ad128897bce5e285aea
jadegong <jadegong@sina.cn>
2026-07-03 20:47:09 +0800

    <feat>(hypr): new hypr shell and idle.

--- hypr/scripts/swayidle.sh
+++ hypr/scripts/swayidle.sh
@@ -2,5 +2,5 @@
 
 # 10分钟熄屏
 exec /usr/bin/swayidle -w \
-  timeout 600 'hyprctl dispatch dpms off' \
-  resume 'hyprctl dispatch dpms on'
+  timeout 600 'hyprctl dispatch "hl.dsp.dpms({ action = \"disable\" })"' \
+  resume 'hyprctl dispatch "hl.dsp.dpms({ action = \"enable\" })"'
