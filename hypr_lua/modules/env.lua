af2b0a53d177c0c80b41707e0112d0bd9bbd4f44
jadegong <jadegong1994@gmail.com>
2026-09-04 15:35:55 +0800

    <feat>(hypr): Cursor for hyprland.

--- hypr_lua/modules/env.lua
+++ hypr_lua/modules/env.lua
@@ -6,8 +6,9 @@
 
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("XCURSOR_THEME", "Dracula-cursors")
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "catppuccin-macchiato-green-cursors")
+hl.env("XCURSOR_SIZE", "28")
+hl.env("HYPRCURSOR_THEME", "catppuccin-macchiato-green-cursors")
+hl.env("HYPRCURSOR_SIZE", "28")
 hl.env("XMODIFIERS", "@im=fcitx")
 hl.env("QT_IM_MODULES", "wayland;fcitx")
